aea3d44f24b9f33c48297b74fedf3fb4278e0e16
simon <fctinstean@gmail.com>
2020-07-03 16:53:01 +0200

    Day 4

---
+++ PHP/Day 4 - Cookies_Sessions_Files/Exercises/toto.php
@@ -0,0 +1,3 @@
+<select multiple size="6"> <option value="American">American flamingo</option> <option value="Andean">Andean flamingo</option> <option value="Chilean">Chilean flamingo</option> <option value="Greater">Greater flamingo</option> <option value="James's">James's flamingo</option> <option value="Lesser">Lesser flamingo</option> </select>
+
+Read more: https://html.com/attributes/select-multiple/#ixzz6FpW790uj
