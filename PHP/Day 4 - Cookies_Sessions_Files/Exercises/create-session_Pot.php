3a3f2b3769ac762b9ae64b348aae0a509ffc47c1
unknown <pgradone@gmail.com>
2020-07-06 16:49:31 +0200

    session exercise 1 done

---
+++ PHP/Day 4 - Cookies_Sessions_Files/Exercises/create-session_Pot.php
@@ -0,0 +1,24 @@
+<?php
+/*
+    Create two pages :
+
+    `create-session.php`
+        The page will initialize a session variable 'viewed' to `true`;
+
+    `get-viewed.php`
+        The page will display a message 'You have visited the page
+        create-session ', if the 'viewed' variable is assigned.
+
+        Otherwise, it will display the message 'You have not visited
+        the create-session page '
+ */
+
+echo 'creating session VIEWED <br><br>';
+
+session_start();
+
+$_SESSION['viewed'] = false;
+
+var_dump($_SESSION);
+// var_dump($_COOKIE);
+// session_destroy();
