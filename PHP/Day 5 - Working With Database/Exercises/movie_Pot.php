857683745942e3d4e6c22ec71827467a117cea0a
unknown <pgradone@gmail.com>
2020-07-07 15:33:11 +0200

    movies PHP_Database_Exercise_1 done until 4

--- PHP/Day 5 - Working With Database/Exercises/movie_Pot.php
+++ PHP/Day 5 - Working With Database/Exercises/movie_Pot.php
@@ -1,6 +1,6 @@
 <?php
 // display welcome
-echo '<h1>Welcome to my amazing movie website<h1>';
+echo '<h1>Movie detail page<h1>';
 
 require_once '../Example Files/database.php';
 // create connection string
@@ -8,53 +8,35 @@ $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD);
 $db_name = 'moviedatabase';
 $db_OK = mysqli_select_db($conn, $db_name);
 
-echo '<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>';
+$movieID = 1;
+
+// var_dump($_GET);
+
+$movieID = $_GET['id'];
+
+// var_dump($movieID);
 
 if ($db_OK) {
-  // echo "$db_name  found !<br>";
-  $qry = 'SELECT movies.*, directors.name, directors.nationality FROM `movies` INNER JOIN directors ON directors.id=movies.director_id';
+  $qry = 'SELECT movies.*, directors.name, directors.nationality 
+    FROM `movies` INNER JOIN directors 
+    ON directors.id=movies.director_id 
+    WHERE movies.id = ' . $movieID;
   $res=mysqli_query($conn,$qry);
 
-  // retrieving the results in an associative array using a loop
-  while ($db_record = mysqli_fetch_assoc($res)) {
+  $movies= mysqli_fetch_all($res,MYSQLI_ASSOC);
+
+  foreach ($movies as $movie) {
     echo '<hr>';
+    echo '<h2>Details of ' . $movie['title'] . '<h2>';
     echo '<figure>
-    <img src="' . $db_record['poster'] . '" alt="' . $db_record['title'] . '" style="width:20rem">
-    <figcaption>' . $db_record['title'] . '</figcaption>
-  </figure><h2> by: ' . $db_record['name'] . ' ('. $db_record['nationality'] . ') ' .  
-  ' released: ' . $db_record['year_released'] . '<h2>';
+    <img src="' . $movie['poster'] . '" alt="' . $movie['title'] . '" style="width:20rem">
+    <figcaption>' . $movie['title'] . '</figcaption>
+    </figure><h3> by: ' . $movie['name'] . ' ('. $movie['nationality'] . ') ' .  
+    ' released: ' . $movie['year_released'] . '<h3>';
   }
 } else {
   echo "$db_name NOT found !<br>";
 }
 
-// var_dump($conn);
-
 mysqli_close($conn);
 
-echo '</body>
-</html>';
-
-?>
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-<figure>
-  <img src="pic_trulli.jpg" alt="Trulli" style="width:100%">
-  <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
-</figure>
-</body>
-</html> -->
