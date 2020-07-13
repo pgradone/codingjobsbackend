ff32c10da4dc627b9564c4c8d160e359e122fdf3
unknown <pgradone@gmail.com>
2020-07-13 15:44:21 +0200

    signin.php - set up registration procedure

--- PHP/Revision Exercises and Projects/Spotify_Project/signin.php
+++ PHP/Revision Exercises and Projects/Spotify_Project/signin.php
@@ -26,27 +26,27 @@ var_dump($_GET)?>
     if(isset($_GET['login']))
       $mode = 'login';
 
-    $errors['mode'] = $mode;
-
+    // *** LOGIN procedure
     if (isset($_POST['login'])) {
       $email = $_POST['mail'];
       $password = $_POST['password'];
-      // sanitizing the email - email is used as user identifier
+      // sanitizing and cleaning the email - email is used as user identifier
       $sanitizedEmail = filter_var(filter_var($email, FILTER_SANITIZE_EMAIL), FILTER_VALIDATE_EMAIL);
       // checking the inputs
       if (!$sanitizedEmail) $errors['email'] = 'Please enter a valid email';
-      if (empty($password)) $errors['email'] = 'Password is missing';
+      if (empty($password)) $errors['password'] = 'Password is missing';
       if (count($errors) == 0) {
-        var_dump($_POST);
         // connect and check if email (user) exists
         require_once 'dbconstants.php';
         $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
         $checkqry = 'SELECT * FROM users WHERE mail = \'' . $sanitizedEmail . '\'' ;
         $errors['SQL'] = $checkqry;
         $result_query = mysqli_query($connection, $checkqry);
+
         $mailusr = mysqli_fetch_assoc($result_query);
 
-        if (is_null($mailusr) && !empty($mailusr)) {
+        // now we can continue checks        
+        if (!is_null($mailusr) && !empty($mailusr)) {
           # verify password for successful login
           if (password_verify($password, $mailusr['password'])) {
             $errors['success'] = $mailusr['first_name'] . ' '
@@ -60,10 +60,54 @@ var_dump($_GET)?>
         } else {
           $errors['emailnotfound'] = 'This user email does not exist - 
             <a>Create ' . $sanitizedEmail . ' ?</a>'; 
-        }        
+        } 
+        mysqli_close($connection);       
       }
-    }      
-  
+    }
+
+    // *** REGISTER procedure
+    if (isset($_POST['register'])) {
+      $email = $_POST['mail'];
+      $password = $_POST['password'];
+      $firstname = $_POST['first_name'];
+      $lastname = $_POST['last_name'];
+      // sanitizing and cleaning the email - email is used as user identifier
+      $sanitizedEmail = filter_var(filter_var($email, FILTER_SANITIZE_EMAIL), FILTER_VALIDATE_EMAIL);
+      // checking the inputs
+      if (!$sanitizedEmail) $errors['email'] = 'Please enter a valid email';
+      if (empty($password)) $errors['password'] = 'Password is missing';
+      if (count($errors) == 0) {
+        // connect and check if email (user) exists
+        require_once 'dbconstants.php';
+        $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+        $checkqry = 'SELECT * FROM users WHERE mail = \'' . $sanitizedEmail . '\'' ;
+        $errors['SQL'] = $checkqry;
+        $result_query = mysqli_query($connection, $checkqry);
+
+        // if existent mail, error, else prepere for insertions
+        $count = mysqli_num_rows($result_query);
+        if ($count > 0) {
+          // EMAIL ALREADY TAKEN !
+          $errors['email'] = 'Email ' . $email . ' already already exists ! <a>Login?</a>';
+        } else {
+          // Hash the password
+          $securePassword = password_hash($password, PASSWORD_DEFAULT);
+          // Prepare query execution
+          $insertquery = "INSERT INTO users(first_name,last_name, mail, password)
+          VALUES('$firstname','$lastname','$sanitizedEmail','$securePassword')";
+          $result_query = mysqli_query($connection, $insertquery);
+          // Check if the user was successfully registered
+          if ($result_query) {
+            $error['registration'] = '$firstname' . ' ' . '$lastname' . ' successfully registered
+            . You can now <a>login</a>.<br>';
+          } else
+            $error['sql'] = 'Something went wrong... Try again. <br>' . $insertquery;
+          // Finally close the connection
+        }
+        mysqli_close($connection);
+      }
+    }
+
   ?>
 
 <h1><?= $mode . ' to the website' ; ?></h1>
