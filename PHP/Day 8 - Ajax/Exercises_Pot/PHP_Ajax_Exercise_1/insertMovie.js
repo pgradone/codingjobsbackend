7ea3f3c560623bc2075ee7b26e515577c38924ca
unknown <pgradone@gmail.com>
2020-07-14 12:00:07 +0200

    start Exercise 1 Ajax

---
+++ PHP/Day 8 - Ajax/Exercises_Pot/PHP_Ajax_Exercise_1/insertMovie.js
@@ -0,0 +1,19 @@
+// called from inside new-movie.html
+// calling insertMovie.php
+
+$function() {
+  $('input[type="submit"]').click(function(e) {
+    e.preventDefault();
+    $ajax({
+      url: 'insertMovie.php',
+      type: 'post',
+      data: $('form').serialize(),
+      success: function(res) {
+        $('#resultMessage').html('<div class="green"> ' + res + ' </div>');
+      },
+      error: function(err) {
+        $('#resultMessage').html('<div class="red"> ' + err + ' </div>');
+      }
+    })
+  })
+}
