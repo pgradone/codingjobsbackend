2d3b8018b5a698036f697230d7ed1358eabfdfa6
Potito Gradone <pgradone@gmail.com>
2020-07-18 14:20:10 +0200

    done exercise 2

--- PHP/Revision Day/Revision_Exercises_Pot/Revision_Exercises_2.php
+++ PHP/Revision Day/Revision_Exercises_Pot/Revision_Exercises_2.php
@@ -32,11 +32,32 @@ echo '<hr>';
 		- The same date than today one month late
 */
 
+echo '<h3>Show the date (in a nice format) for :<br>
+	- Today: ' . date('d/m/Y', strtotime('Today')) . '<br>
+	- Tomorrow ' . date('d/m/Y', strtotime('Tomorrow')) . '<br>
+	- The same date than today one month late: '
+	. date('d/m/Y', strtotime('Next Month')) . '<br></h3>';
+
+echo '<hr>';
 /*
 	Create an array with the numbers from 1 to 100
 	Create it dynamically using a loop.
 	Display it in a HTML list (ul/li)
 */
+
+echo '<h3>Create an array with the numbers from 1 to 100<br>
+Create it dynamically using a loop.</h3><br>';
+$numberArray = [];
+for ($i = 1; $i <= 100; $i++) {
+	$numberArray[$i] = $i;
+}
+$display = '<ul><li>' . implode('</li><li>', $numberArray) . '</li></ul>';
+// var_dump($display);
+
+echo '<h3>Display it in a HTML list (ul/li)</h3>';
+// echo $display;
+
+echo '<hr>';
 
 /*
 	Using the moviedb :
@@ -45,3 +66,27 @@ echo '<hr>';
 	3. Retrieve the number of rows affected by this query
 
 */
+
+echo '<h3>1. Connect to the database and choose the moviedb</h3>';
+$db_name = 'moviedb';
+$db_handle = mysqli_connect('localhost', 'root', '', $db_name);
+$db_found = mysqli_select_db($db_handle, $db_name);
+if ($db_found) {
+	echo 'Connection : ' . $db_handle . '<br>';
+	// build the query
+	echo '<h3>2. Run a query to delete all movies by George Lucas</h3>';
+	$sql_txt = 'SELECT m.id from movies m '
+		. 'INNER JOIN directors d ON m.director_id = d.id '
+		. 'WHERE d.name = \'George Lucas\'';
+	$result_query = mysqli_query($db_handle, $sql_txt);
+
+	if ($result_query) {
+		// $movies = mysqli_fetch_all($result_query, MYSQLI_ASSOC);
+		echo 'SQL QUERY : ' . $sql_txt . '<br>';
+		echo '<h3>3. Retrieve the number of rows affected by this query</h3><br>';
+		$affectedrows = mysqli_affected_rows($db_handle);
+		echo $affectedrows;
+	} else {
+		echo 'Problem with ' . $sql_txt;
+	}
+}
