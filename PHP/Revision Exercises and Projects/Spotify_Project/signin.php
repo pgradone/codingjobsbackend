f1e87dcfcff2a05d5566c6d311f353856a9a009b
Potito Gradone <pgradone@gmail.com>
2020-07-25 14:51:20 +0200

    improve register UX

--- PHP/Revision Exercises and Projects/Spotify_Project/signin.php
+++ PHP/Revision Exercises and Projects/Spotify_Project/signin.php
@@ -19,13 +19,20 @@
   $email = '';
   $loginEmail = '';
   $errors = [];
+  $messages = [];
   $mode = 'login';
   $extraFields = '';
+
+  var_dump($_GET);
+
   if (isset($_GET['register'])) {
     $mode = 'register';
     $extraFields = '<br>- First Name :<input type="text" name="first_name" placeholder="First Name"><br>
       - Last Name :<input type="text" name="last_name" placeholder="Last_Name">';
+    # set default email login if present from register request
+    $loginEmail = !empty($_GET['register']) ? $_GET['register'] : '';
   }
+
   if (isset($_GET['login'])) {
     $mode = 'login';
     $loginEmail = $_GET['login'];
@@ -35,8 +42,9 @@
     $mode = 'login';
     $loginEmail = $_GET['logout'];
     if (isset($_SESSION['emailuser'])) {
-      $error['logoutOK'] = $_SESSION['emailuser'] . ' successfully logged out';
+      $messages['logoutOK'] = $_SESSION['emailuser'] . ' successfully logged out';
       session_unset();
+      header('Refresh: 5; url="signin.php');
     }
   }
 
@@ -46,15 +54,15 @@
     $email = $_POST['mail'];
     $password = $_POST['password'];
     // sanitizing and cleaning the email - email is used as user identifier
-    $sanitizedEmail = filter_var(filter_var($email, FILTER_SANITIZE_EMAIL), FILTER_VALIDATE_EMAIL);
+    $validEmail = filter_var(filter_var($email, FILTER_SANITIZE_EMAIL), FILTER_VALIDATE_EMAIL);
     // checking the inputs
-    if (!$sanitizedEmail) $errors['email'] = 'Please enter a valid email';
+    if (!$validEmail) $errors['email'] = 'Please enter a valid email';
     if (empty($password)) $errors['password'] = 'Password is missing';
     if (count($errors) == 0) {
       // connect and check if email (user) exists
       require_once 'dbconstants.php';
       $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
-      $checkqry = 'SELECT * FROM users WHERE mail = \'' . $sanitizedEmail . '\'';
+      $checkqry = 'SELECT * FROM users WHERE mail = \'' . $validEmail . '\'';
       $errors['SQL'] = $checkqry;
       $result_query = mysqli_query($connection, $checkqry);
 
@@ -64,7 +72,7 @@
       if (!is_null($mailusr) && !empty($mailusr)) {
         # verify password for successful login
         if (password_verify($password, $mailusr['password'])) {
-          $errors['success'] = $mailusr['first_name'] . ' '
+          $messages['success'] = $mailusr['first_name'] . ' '
             . $mailusr['last_name'] . ' is successfully logged in! <br>
             ...redirecting to account page.. <a href="signin.php?logout">Logout</a>';
           $_SESSION['emailuser'] = $mailusr['mail'];
@@ -77,8 +85,8 @@
           $errors['wrongpassword'] = 'Invalid password.';
         }
       } else {
-        $errors['emailnotfound'] = 'This user email does not exist - 
-            <a href="signin?register:' . $sanitizedEmail . '">Register ' . $sanitizedEmail . ' ?</a>';
+        $messages['emailnotfound'] = 'This user email does not exist - 
+            <a href="signin?register=' . $validEmail . '">Register ' . $validEmail . ' ?</a>';
       }
       mysqli_close($connection);
     }
@@ -91,19 +99,20 @@
     $firstname = $_POST['first_name'];
     $lastname = $_POST['last_name'];
     // sanitizing and cleaning the email - email is used as user identifier
-    $sanitizedEmail = filter_var(filter_var($email, FILTER_SANITIZE_EMAIL), FILTER_VALIDATE_EMAIL);
+    $validEmail = filter_var(filter_var($email, FILTER_SANITIZE_EMAIL), FILTER_VALIDATE_EMAIL);
     // checking the inputs
-    if (!$sanitizedEmail) $errors['email'] = 'Please enter a valid email';
+    if (!$validEmail) $errors['email'] = 'Please enter a valid email';
     if (empty($password)) $errors['password'] = 'Password is missing';
+    if (empty($firstname)) $errors['firstname'] = 'enter at least a first name';
     if (count($errors) == 0) {
       // connect and check if email (user) exists
       require_once 'dbconstants.php';
       $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
-      $checkqry = 'SELECT * FROM users WHERE mail = \'' . $sanitizedEmail . '\'';
-      // $errors['SQL'] = $checkqry;
+      $checkqry = 'SELECT * FROM users WHERE mail = \'' . $validEmail . '\'';
+      $messages['SQL'] = $checkqry;
       $result_query = mysqli_query($connection, $checkqry);
 
-      // if existent mail, error, else prepere for insertions
+      // if existent mail, error, else prepare for insertions
       $count = mysqli_num_rows($result_query);
       if ($count > 0) {
         // EMAIL ALREADY TAKEN !
@@ -113,13 +122,14 @@
         $securePassword = password_hash($password, PASSWORD_DEFAULT);
         // Prepare query execution
         $insertquery = "INSERT INTO users(first_name,last_name, mail, password)
-          VALUES('$firstname','$lastname','$sanitizedEmail','$securePassword')";
-        //  $errors['SQL'] = $insertquery;
+          VALUES('$firstname','$lastname','$validEmail','$securePassword')";
+        $messages['SQL'] = $insertquery;
         $result_query = mysqli_query($connection, $insertquery);
         // Check if the user was successfully registered
         if ($result_query) {
-          $error['registrationOK'] = '$firstname' . ' ' . '$lastname' . ' successfully registered
-            . You can now <a href="">login</a>.<br>';
+          $messages['registrationOK'] = '$firstname' . ' ' . '$lastname' . ' successfully registered
+            . <a href="">Login...</a>.<br>';
+          header('Refresh: 5; url="account.php');
         } else
           $error['dberror'] = 'Something went wrong... Try again. <br>' . $insertquery;
         // Finally close the connection
@@ -141,7 +151,10 @@
   <?php
   echo '<h2>Status :<h2>';
   foreach ($errors as $key => $error)
-    echo " <h3> $key : $error <h3>";
+    echo " <h3 style=\"color:red\"> $key : $error <h3>";
+  foreach ($messages as $key => $message)
+    echo " <h3 style=\"color:green\"> $key : $message <h3>";
+
   ?>
 
 
