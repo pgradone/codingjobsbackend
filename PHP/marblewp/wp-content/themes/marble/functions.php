72fb5d5f55c4035eed8b7e3a4e0a7cb79a8a9db7
unknown <pgradone@gmail.com>
2020-07-27 13:04:38 +0200

    copy features from twentytwenty theme

--- PHP/marblewp/wp-content/themes/marble/functions.php
+++ PHP/marblewp/wp-content/themes/marble/functions.php
@@ -21,18 +21,62 @@ function marble_styles_and_scripts()
     get_template_directory_uri() . '/css/styles.css',
     array('marble-normalize')
   );
-  
 }
 //call for the function whenever Wordpress enqueues its own scripts and styles 
 add_action('wp_enqueue_scripts', 'marble_styles_and_scripts');
 
-
 /**
  * Theme supported features declaration
  */
-function marble_theme_support(){
-  
+function marble_theme_support()
+{
+  // Add default posts and comments RSS feed links to head.
+  add_theme_support('automatic-feed-links');
+
+  /*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+  add_theme_support('post-thumbnails');
+
+  // Set post thumbnail size.
+  //set_post_thumbnail_size( 1200, 9999 );
+  // Add custom image size used in Cover Template.
+  //add_image_size( 'twentytwenty-fullscreen', 1980, 9999 );
+
+
+  /*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+  add_theme_support('title-tag');
+
+  /*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+  add_theme_support(
+    'html5',
+    array(
+      'search-form',
+      'comment-form',
+      'comment-list',
+      'gallery',
+      'caption',
+      'script',
+      'style',
+    )
+  );
+
+  /*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on Twenty Twenty, use a find and replace
+	 * to change 'twentytwenty' to the name of your theme in all the template files.
+	 */
+  load_theme_textdomain('marble');
 }
-add_action( 'after_setup_theme', 'marble_theme_support' );
-
-
+add_action('after_setup_theme', 'marble_theme_support');
