d6c76304b417398fb68b899a41c7fd1ecdc5b606
unknown <pgradone@gmail.com>
2020-07-27 12:47:12 +0200

    fix bugs, add dependency array()

--- PHP/marblewp/wp-content/themes/marble/functions.php
+++ PHP/marblewp/wp-content/themes/marble/functions.php
@@ -1,20 +1,26 @@
 <?php
 
-//functions.php :
+/**
+ * function to enqueue my styles and scripts in the template head
+ */
+function marble_styles_and_scripts()
+{
+  //include the scripts and styles of the theme
+  wp_enqueue_style(
+    'marble-normalize',
+    get_template_directory_uri() . '/css/normalize.css'
+  );
 
+  wp_enqueue_style(
+    'marble-font',
+    'https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed:400,700|Roboto+Slab:400,700'
+  );
 
-
-//Exercice: create an action to add the 2 required styles for this theme
-// using the wp_enqueue_style() function with the correct add_action() statement
-
-
-// get_template_directory_uri() . '/css/normalize.css'
-// https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed:400,700|Roboto+Slab:400,700
-// get_template_directory_uri() . '/css/styles.css'
-
-function wp_enqueue_stylesandscripts() {
-  wp_enqueue_style( 'marble-normalize', get_template_directory_uri() . '/css/normalize.css');
-  wp_enqueue_style( 'marble-fonts', 'https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed:400,700|Roboto+Slab:400,700');
-  wp_enqueue_style( 'marble-styles', get_template_directory_uri() . '/css/styles.css');
+  wp_enqueue_style(
+    'marble-styles',
+    get_template_directory_uri() . '/css/styles.css',
+    array('marble-normalize')
+  );
 }
-add_action( 'wp_enqueue_scripts', 'wp_enqueue_stylesandscripts' );
+//call for the function whenever Wordpress enqueues its own scripts and styles 
+add_action('wp_enqueue_scripts', 'marble_styles_and_scripts');
