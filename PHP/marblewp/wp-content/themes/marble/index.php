bd3b4638c87ff71cdd16db9ba15af5b1edf26499
unknown <pgradone@gmail.com>
2020-07-27 17:03:47 +0200

    refine main loop

--- PHP/marblewp/wp-content/themes/marble/index.php
+++ PHP/marblewp/wp-content/themes/marble/index.php
@@ -2,18 +2,29 @@
 
 <section>
 	<div class="wrapper">
-		<?php 
-            if ( have_posts() ) {
-                while ( have_posts() ) {
-                    the_post(); 
-                    //inside de loop
-                    // You can use template tags here to display post informations
-                  	the_title();
-                    
-                    
-                } // end while
-            } // end if
-            ?>
+		<?php
+		//if there is something to display...
+		if (have_posts()) {
+			//for each post...
+			while (have_posts()) {
+				//set the curent post in the loop as the content
+				the_post();
+
+				// inside de loop
+				// You can use template tags here to display post informations
+				the_title('<h1>', '</h1>');
+
+				echo '<p>' . get_the_author() . '</p>';
+
+
+				if (is_singular()) :
+					the_content();
+				else :
+					the_excerpt();
+				endif;
+			} // end while
+		} // end if
+		?>
 	</div>
 </section>
 
