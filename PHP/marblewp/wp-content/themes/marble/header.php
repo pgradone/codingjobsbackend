cadf952b770d9c474ac2ed354c455fc090273aa4
unknown <pgradone@gmail.com>
2020-07-27 11:40:27 +0200

    add extra tags for wp hendling into header.php

--- PHP/marblewp/wp-content/themes/marble/header.php
+++ PHP/marblewp/wp-content/themes/marble/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 <head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Marble - homepage</title>
@@ -12,9 +13,12 @@
 	
 	<!-- mes styles -->
 	<link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/css/styles.css">
+  <!-- this is mandatory -->
   <?php wp_head(); ?>
 </head>
 <body>
+
+<body <?php body_class(); ?>>
 
 	<header class="main-header">
 		<div class="wrapper">
