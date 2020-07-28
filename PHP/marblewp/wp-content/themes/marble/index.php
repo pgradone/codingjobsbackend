c8484ee9d361bd904ab8d5b83b8f59f87c2675f7
unknown <pgradone@gmail.com>
2020-07-28 11:25:54 +0200

    displace post and page in different files, for using the get_template_part function

--- PHP/marblewp/wp-content/themes/marble/index.php
+++ PHP/marblewp/wp-content/themes/marble/index.php
@@ -13,50 +13,14 @@
 				//set the curent post in the loop as the content
 				the_post();
 
-				//a single post
-				if (is_single()) {
-		?>
-					<div class="post-entry">
-						<div class="post-thumbnail">
-							<?php the_post_thumbnail('medium'); ?>
-						</div>
-						<div class="post-content">
-							<h2><?php the_title(); ?></h2>
-							<p class="post-metas"><?php the_category(', '); ?><?= ' - ' . get_the_author() . ' - ' . get_the_date(); ?></p>
-							<p><?php the_content(); ?></p>
-						</div>
-					</div>
-				<?php
-					//a single page
-				} else if (is_page()) {
-				?>
-					<div class="post-entry">
-						<h2><?php the_title(); ?></h2>
-						<div class="post-content"><?php the_content(); ?></div>
-					</div>
-				<?php
-					//a list of posts
-				} else {
-				?>
-					<div class="post-entry">
-						<div class="post-thumbnail">
-							<?php the_post_thumbnail('medium'); ?>
-						</div>
-						<div class="post-content">
-							<h2><?php the_title(); ?></h2>
-							<p class="post-metas"><?php the_category(', '); ?><?= ' - ' . get_the_author() . ' - ' . get_the_date(); ?></p>
-							<p><?php the_excerpt(); ?></p>
-							<p><a href="<?php the_permalink(); ?>">Read more</a></p>
-						</div>
-					</div>
-		<?php
-				}
+				get_template_part('template-parts/content', get_post_type());
+				//template-parts/content-post.php
+				//template-parts/content-page.php
 
-				echo '<br>';
 			} // end while
 		} // end if
 		?>
 	</div>
 </section>
 
-<?php get_footer(); ?>
+<?php get_footer(); ?>>
---
+++ PHP/marblewp/wp-content/themes/marble/template-parts/content-page.php
@@ -0,0 +1,6 @@
+//template-parts/content-page.php
+
+<div class="post-entry">
+  <h2><?php the_title(); ?></h2>
+  <div class="post-content"><?php the_content(); ?></div>
+</div>
