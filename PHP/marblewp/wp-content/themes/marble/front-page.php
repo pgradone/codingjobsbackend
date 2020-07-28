2aff6e830a89d5980e2b6e8c75dbc6184ea60d57
unknown <pgradone@gmail.com>
2020-07-28 10:08:26 +0200

    exercise: front page to adapt like the index

--- PHP/marblewp/wp-content/themes/marble/front-page.php
+++ PHP/marblewp/wp-content/themes/marble/front-page.php
@@ -1,74 +1,75 @@
-<?php get_header(); 
+//front-page.php
+//http://sharemycode.fr/0wr
 
-// reference : https://wphierarchy.com/
-?>
 
-		<section class="jumbotron">
-			<div class="wrapper">
-				<h2>We are digital &amp; branding agency based in London.</h2>
-				<h3>We love to turn great ideas into beautiful products.</h3>
-				<a href="#" class="button">See portfolio</a>
-			</div>
-		</section>
-		<!-- ./jumbotron -->
+<?php get_header(); ?>
 
-		
-		<section id="section-icons" class="wrapper">
-			<div class="container">
-				<div class="col">
-					<i class="icon lamp"></i>
-					<h4>Pellentesque</h4>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quasi facere, animi maxime natus cupiditate</p>
-				</div>
-				<!-- ./col1 -->
-				<div class="col">
-					<i class="icon clock"></i>
-					<h4>Consectetur</h4>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quasi facere, animi maxime natus cupiditate</p>
-				</div>
-				<!-- ./col2 -->
-				<div class="col">
-					<i class="icon flask"></i>
-					<h4>Tristiquet</h4>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quasi facere, animi maxime natus cupiditate</p>
-				</div>
-				<!-- ./col3 -->
-				<div class="col">
-					<i class="icon ticket"></i>
-					<h4>Fermentum</h4>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quasi facere, animi maxime natus cupiditate</p>
-				</div>
-				<!-- ./col4 -->
-			</div>
+<section class="jumbotron">
+  <div class="wrapper">
+    <h2>We are digital &amp; branding agency based in London.</h2>
+    <h3>We love to turn great ideas into beautiful products.</h3>
+    <a href="#" class="button">See portfolio</a>
+  </div>
+</section>
+<!-- ./jumbotron -->
 
-			<hr />
 
-		</section>
-		
-		<section id="section-latest-work" class="wrapper">
-			<h3>Our latest works</h3>
-			<div class="container">
-				<article class="col">
-					<img src="<?= get_template_directory_uri(); ?>/img/image1.jpg" alt="Business Card">
-					<h4>Nobis Business Card</h4>
-					<h5>Business Cards, Graphics</h5>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-				</article>
+<section id="section-icons" class="wrapper">
+  <div class="container">
+    <div class="col">
+      <i class="icon lamp"></i>
+      <h4>Pellentesque</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quasi facere, animi maxime natus cupiditate</p>
+    </div>
+    <!-- ./col1 -->
+    <div class="col">
+      <i class="icon clock"></i>
+      <h4>Consectetur</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quasi facere, animi maxime natus cupiditate</p>
+    </div>
+    <!-- ./col2 -->
+    <div class="col">
+      <i class="icon flask"></i>
+      <h4>Tristiquet</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quasi facere, animi maxime natus cupiditate</p>
+    </div>
+    <!-- ./col3 -->
+    <div class="col">
+      <i class="icon ticket"></i>
+      <h4>Fermentum</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quasi facere, animi maxime natus cupiditate</p>
+    </div>
+    <!-- ./col4 -->
+  </div>
 
-				<article class="col">
-					<img src="<?= get_template_directory_uri(); ?>/img/image2.jpg" alt="New fun project">
-					<h4>New fun project</h4>
-					<h5>Webdesign, Application</h5>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-				</article>
-				
-				<article class="col">
-					<img src="<?= get_template_directory_uri(); ?>/img/image3.jpg" alt="Passionaries Branding Cover">
-					<h4>Passionaries Branding Cover</h4>
-					<h5>Branding, Graphic Design</h5>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-				</article>
-			</div>
-		</section>
+  <hr />
 
-		<?php get_footer(); ?>
+</section>
+
+<section id="section-latest-work" class="wrapper">
+  <h3>Our latest works</h3>
+  <div class="container">
+    <article class="col">
+      <img src="<?= get_template_directory_uri(); ?>/img/image1.jpg" alt="Business Card">
+      <h4>Nobis Business Card</h4>
+      <h5>Business Cards, Graphics</h5>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </article>
+
+    <article class="col">
+      <img src="<?= get_template_directory_uri(); ?>/img/image2.jpg" alt="New fun project">
+      <h4>New fun project</h4>
+      <h5>Webdesign, Application</h5>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </article>
+
+    <article class="col">
+      <img src="<?= get_template_directory_uri(); ?>/img/image3.jpg" alt="Passionaries Branding Cover">
+      <h4>Passionaries Branding Cover</h4>
+      <h5>Branding, Graphic Design</h5>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </article>
+  </div>
+</section>
+
+<?php get_footer(); ?>
