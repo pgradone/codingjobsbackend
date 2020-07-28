32c07eb349322bb322aeb96a835bc2f309e7f55c
unknown <pgradone@gmail.com>
2020-07-28 09:39:46 +0200

    example of conditional tags

--- PHP/marblewp/wp-content/themes/marble/index.php
+++ PHP/marblewp/wp-content/themes/marble/index.php
@@ -3,12 +3,16 @@
 <section>
 	<div class="wrapper">
 		<?php
+		
 		//if there is something to display...
 		if (have_posts()) {
+
 			//for each post...
 			while (have_posts()) {
+
 				//set the curent post in the loop as the content
 				the_post();
+
 
 				// inside de loop
 				// You can use template tags here to display post informations
@@ -16,10 +20,13 @@
 
 				echo '<p>' . get_the_author() . '</p>';
 
-
+				//example conditional tag:
+				//if the page shown is a singular element (page or post) :
 				if (is_singular()) :
+					//display the full content of the post/page
 					the_content();
 				else :
+					//else: display only the resume
 					the_excerpt();
 				endif;
 			} // end while
