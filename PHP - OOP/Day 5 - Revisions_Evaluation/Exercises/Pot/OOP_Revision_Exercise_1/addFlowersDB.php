0e4145d9df2a1ad80e469e691bcbd26f05e30bf4
Potito Gradone <pgradone@gmail.com>
2020-08-03 19:59:46 +0200

    finish flower exercise

--- PHP - OOP/Day 5 - Revisions_Evaluation/Exercises/Pot/OOP_Revision_Exercise_1/addFlowersDB.php
+++ PHP - OOP/Day 5 - Revisions_Evaluation/Exercises/Pot/OOP_Revision_Exercise_1/addFlowersDB.php
@@ -9,52 +9,51 @@ $price = '';
 // var_dump($_POST);
 
 if (!empty($_POST)) {
-// if (isset($_POST['submit'])) {
-    $errors = array();
-    $messages = [];
-    if (empty($_POST['name'])) {
-        $errors['name'] = 'Name cannot be empty!';
-    } else {
-        $name = $_POST['name'];
-    }
-    if (empty($_POST['price'])) {
-      $errors['price'] = 'Price cannot be empty!';
-  } 
-    if (!is_numeric($_POST['price'])) {
-      $errors['price type'] = 'Price must be numeric';
-    } else {
-      $price = $_POST['price'];
-    }
-    if (sizeof($errors) == 0) {
-      $messages['Form OK'] = 'input fields are OK';
-      $pdo = dbConnect();
-      if ($pdo) {
-        $messages['DB OK'] = 'DB connection OK';
-        $sqltxt = 'INSERT INTO flowers (name, price) VALUES (?,?)';
-        $prep = $pdo->prepare($sqltxt);
-        $prep->bindValue(1,$name, PDO::PARAM_STR);
-        $prep->bindValue(2,$price, PDO::PARAM_INT);
-        $sqlCompleteTxt = 'INSERT INTO flowers (name, price) VALUES (' . $name . ',' . $price . ')';
-        $sqlCompleteTxt = $prep->queryString;
-        $res = $prep->execute();
-        if ($res && $prep->rowCount() > 0) {
-          $messages['INSERT OK'] = 'new flower created :)';
-          echo 'Flower ' . $name . ' successfully added!';
-        } else {
-          $errors['Insert error'] = 'could not insert with query: ' . $sqlCompleteTxt;
-          echo 'Flower not inserted with query :' . $sqlCompleteTxt ;
-        }      
-      } else {
-        $messages['DBNOK'] = 'Could not connect to flowers DB';
-      }
-  
-    } else {
-      $messages = [];
-      $messages['Redo imput'] = 'awaiting input....';
-    }
+  // if (isset($_POST['submit'])) {
+  $errors = array();
+  $messages = [];
+  if (empty($_POST['name'])) {
+    $errors['name'] = 'Name cannot be empty!';
+  } else {
+    $name = $_POST['name'];
   }
+  if (empty($_POST['price'])) {
+    $errors['price'] = 'Price cannot be empty!';
+  }
+  if (!is_numeric($_POST['price'])) {
+    $errors['price type'] = 'Price must be numeric';
+  } else {
+    $price = $_POST['price'];
+  }
+  if (sizeof($errors) == 0) {
+    $messages['Form OK'] = 'input fields are OK';
+    $pdo = dbConnect();
+    if ($pdo) {
+      $messages['DB OK'] = 'DB connection OK';
+      $sqltxt = 'INSERT INTO flowers (name, price) VALUES (?,?)';
+      $prep = $pdo->prepare($sqltxt);
+      $prep->bindValue(1, $name, PDO::PARAM_STR);
+      $prep->bindValue(2, $price, PDO::PARAM_INT);
+      $sqlCompleteTxt = 'INSERT INTO flowers (name, price) VALUES (' . $name . ',' . $price . ')';
+      $res = $prep->execute();
+      if ($res && $prep->rowCount() > 0) {
+        $messages['INSERT OK'] = 'new flower created :)';
+        echo 'Flower ' . $name . ' successfully added!';
+      } else {
+        $errors['Insert error'] = 'could not insert with query: ' . $sqlCompleteTxt;
+        echo 'Flower not inserted with query :' . $sqlCompleteTxt;
+      }
+    } else {
+      $messages['DBNOK'] = 'Could not connect to flowers DB';
+    }
+  } else {
+    $messages = [];
+    $messages['Redo imput'] = 'awaiting input....';
+  }
+}
 
-function dbConnect() {
+function dbConnect()
+{
   $pdo = null;
   $connString = 'mysql:host=localhost;dbname=flowers';
   $pdo = new PDO($connString, 'root', '');
