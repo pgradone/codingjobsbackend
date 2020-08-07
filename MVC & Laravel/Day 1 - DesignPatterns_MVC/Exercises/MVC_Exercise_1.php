3de2fa6241aae4f5557b26a72e494d2026facab5
InsteanAzeros <fctinstean@gmail.com>
2020-08-07 12:28:28 +0200

    MVC Exercise

---
+++ MVC & Laravel/Day 1 - DesignPatterns_MVC/Exercises/MVC_Exercise_1.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+
+******* Exercise : Flower Website ********
+
+
+In the Flowers table, we will record: The id, the name of the flowers, the price but also a nice picture
+
+In this exercise, you have to 
+    - Create a page where you display the list of flowers (display Name and price). With a link 'read more'.
+    This link will bring you to the detail page of the flower.
+    - Create a page where you display the detail of a specific flower. 
+    In this page you should first print the picture then the rest of informations.
+
+
+You must use MVC and OOP !!
+
+So you have to create a FlowerController that ask the FlowerModel to return the flowers !
+You probably have to create the Flower class also.
+Everything will be display in the Views.
+
+
+Next part of this exercise : See MVC_EXERCISE_2.php
