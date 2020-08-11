3c58a1b65a211be085c103a9f3254da7f1b3bddd
pgradone <pgradone@gmail.com>
2020-08-11 17:59:48 +0200

    update the main router index.php with calls to the BooksController method

--- MVC & Laravel/BookStore/routes/web.php
+++ MVC & Laravel/BookStore/routes/web.php
@@ -13,14 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Welcome screen
+
 Route::get('/', function () {
     // return view('welcome');
     return view('welcome-books');
 });
 
-Route::get('/books', 'BooksController@index')->name('books');
+// C.R.U.D -> Create Read Update Delete
 
-/* Route::get('/books', function() {
-    return 'List of books';
-});
- */
+
+// **** CREATE SECTION
+// Show the creation form only : 
+Route::get('/books/create', 'BooksController@create');
+// Process submitted form :
+  Route::post('/books/create', 'BooksController@store');
+  
+// **** READ SECTION
+// List ALL the books :
+Route::get('/books', 'BooksController@index')->name('books');
+// Display a specific book : 
+Route::get('/books/{id}', 'BooksController@show');
+    
+// **** UPDATE SECTION
+// Show the form only : 
+  Route::get('/books/edit/{id}', 'BooksController@edit')->name('book.edit');
+// Process submitted form :
+  Route::put('/books/edit/{id}', 'BooksController@update');
+    
+// **** DELETE SECTION
+// Delete one specific book :
+Route::delete('/books/delete/{id}', 'BooksController@destroy');
